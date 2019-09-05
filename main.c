2bbe93a1b42d1d6e065f3edf07f997858eedf745
mmonahan <dr.mirkops@gmail.com>
2019-09-05 21:04:35 +0300

    boom!

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 16:27:34 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/09/04 20:43:59 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/09/05 21:00:24 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,24 @@ int main()
 		int			i;
 		char		*str;
 		char		*stop = "Phibonacci: error: invalid map";
-		char		*p1 = "$$$ exec p1 : [players/mmonahan.filler]";
-		char		*p2 = "$$$ exec p2 : [players/mmonahan.filler]";
+		char		*p1 = "$$$ exec p1 : [./mmonahan.filler]";
 
 		str = "abc";
-		p.last = 0;
-		p.first = 0;
-		while (ft_strcmp(str, stop)) //(i > 0)
+		p.first = 'X';
+		p.last = 'x';
+		while (1)//(ft_strcmp(str, stop)) //(i > 0)
 		{
 			i = get_next_line(0, &str);
+			if (!ft_strcmp(str, stop))
+				return (1);
 			if (!ft_strcmp(str, p1))
 			{
 				p.first = 'O';
 				p.last = 'o';
 			}
-			else if (!ft_strcmp(str, p2))
-			{
-				p.first = 'X';
-				p.last = 'x';
-			}
-			printf("gnl = %d\t **line = %s\n", i, str);
+
+			//printf("gnl = %d\t **line = %s\n", i, str);
+			write(1, "8 2\n", 4);
 		}
 	}
 
