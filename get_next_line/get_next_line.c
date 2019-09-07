5679810142b6bfa4c173270959656c1d416ed686
mmonahan <dr.mirkops@gmail.com>
2019-09-07 18:31:47 +0300

    pravki gnl...

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 21:05:18 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/02/08 16:29:51 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/09/07 18:30:05 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,11 +66,79 @@ int	get_next_line(const int fd, char **line)
 		if (ft_strchr(m[fd], '\n') != NULL || ret != BUFF_SIZE)
 			break ;
 	}
-	*line = ft_strchr(m[fd], '\n') ? ft_strsub(m[fd], 0, ft_charcount(m[fd],
-		'\n')) : ft_strsub(m[fd], 0, ft_strlen(m[fd]) + 1);
+	*line = ft_strchr(m[fd], '\n') ? ft_strsub(m[fd], 0, ft_charcount(m[fd],'\n')) :ft_strsub(m[fd], 0, ft_strlen(m[fd]) + 1);
 	tmp = m[fd];
-	m[fd] = ft_strsub(tmp, ft_charcount(tmp, '\n') + 1, ft_strlen(tmp) -
-		ft_charcount(tmp, '\n'));
+	m[fd] = ft_strsub(tmp, ft_charcount(tmp, '\n') + 1, ft_strlen(tmp) - ft_charcount(tmp, '\n'));
 	ft_strdel(&tmp);
 	return (!(!ret && !ft_strlen(*line)));
 }
+
+
+//
+//#include <stdio.h>
+//
+//int		line_verif(char **line, char **tmp, int res, char **str)
+//{
+//	*str = NULL;
+//	if (res == 0 && ft_strlen(*tmp) > 0)
+//	{
+//		*line = *tmp;
+//		*tmp = NULL;
+//		return (1);
+//	}
+//	return (res);
+//}
+//
+//char	*read_line(char *tmp)
+//{
+//	int		t;
+//	char	*line;
+//
+//	t = 0;
+//	while (tmp[t] != '\n')
+//		t++;
+//	line = (char *)malloc((t + 1) * sizeof(char));
+//	line = ft_strncpy(line, tmp, t);
+//	line[t] = '\0';
+//	return (line);
+//}
+//
+//char	*cpycat(char *s1, char *s2)
+//{
+//	char	*tmp;
+//
+//	tmp = NULL;
+//	tmp = ft_memalloc(ft_strlen(s1) + ft_strlen(s2));
+//	s1 ? tmp = ft_strcpy(tmp, s1) : NULL;
+//	s1 ? tmp = ft_strncat(tmp, s2, ft_strlen(s2)) : NULL;
+//	//printf("%s\n", tmp);
+//	return (tmp);
+//}
+//
+//int		get_next_line(int const fd, char **line)
+//{
+//	static char		*str = NULL;
+//	int				res;
+//	char			*buf;
+//	char			*tmp;
+//
+//	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 10000000)
+//		return (-1);
+//	buf = ft_strnew(BUFF_SIZE + 1);
+//	if (str == NULL)
+//		str = ft_memalloc(BUFF_SIZE);
+//	tmp = ft_strncpy(ft_memalloc(BUFF_SIZE), str, BUFF_SIZE);
+//	while (!(ft_strchr(tmp, '\n')))
+//	{
+//		if ((res = read(fd, buf, BUFF_SIZE)) < 1)
+//			return (line_verif(line, &tmp, res, &str));
+//		buf[res] = '\0';
+//		tmp = cpycat(tmp, buf);
+//	}
+//	*line = read_line(tmp);
+//	if (ft_strchr(tmp, '\n'))
+//		str = ft_strncpy(str, ft_strchr(tmp, '\n') + 1, BUFF_SIZE);
+//	free(tmp);
+//	free(buf);
+//	return (1);
+//}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 16:27:34 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/09/05 21:00:24 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/09/07 18:18:16 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,37 @@ int main()
 		t_player 	p;
 		int			i;
 		char		*str;
+		int			fd;
 		char		*stop = "Phibonacci: error: invalid map";
 		char		*p1 = "$$$ exec p1 : [./mmonahan.filler]";
 
 		str = "abc";
 		p.first = 'X';
 		p.last = 'x';
-		while (1)//(ft_strcmp(str, stop)) //(i > 0)
+
+		fd = open("test.txt", O_WRONLY);
+
+//		get_next_line(0, &str);
+//		if (!ft_strcmp(str, p1))
+//		{
+//			p.first = 'O';
+//			p.last = 'o';
+//		}
+		while (1)
 		{
-			i = get_next_line(0, &str);
+			get_next_line(0, &str);
+
+			i = ft_strlen(str);
 			if (!ft_strcmp(str, stop))
 				return (1);
-			if (!ft_strcmp(str, p1))
-			{
-				p.first = 'O';
-				p.last = 'o';
-			}
 
-			//printf("gnl = %d\t **line = %s\n", i, str);
+
+//			printf("gnl = %d\t **line = %s\n", i, str);
 			write(1, "8 2\n", 4);
+			write(fd, str, i);
+			write(fd, "\n", 1);
 		}
+		close(fd);
 	}
 
 	return 0;
